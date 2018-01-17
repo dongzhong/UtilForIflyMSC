f19e6c182dbcf8a070c58639cfe981d97b1f04e1
董重 <zhong.dong@flyingwings.cn>
2018-01-17 15:01:50 +0800

    MainActivity界面

--- app/src/main/java/dongzhong/utilforiflymsc/MainActivity.java
+++ app/src/main/java/dongzhong/utilforiflymsc/MainActivity.java
@@ -2,12 +2,36 @@ package dongzhong.utilforiflymsc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        textView = (TextView) findViewById(R.id.textview_result);
+        button = (Button) findViewById(R.id.button_start);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_start:
+                break;
+            default:
+                break;
+        }
     }
 }
