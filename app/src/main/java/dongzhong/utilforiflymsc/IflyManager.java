49a04e9b1b3635bf528193e62ff98a4bf976457d
董重 <zhong.dong@flyingwings.cn>
2018-01-17 20:38:26 +0800

    save

--- app/src/main/java/dongzhong/utilforiflymsc/IflyManager.java
+++ app/src/main/java/dongzhong/utilforiflymsc/IflyManager.java
@@ -1,9 +1,15 @@
 package dongzhong.utilforiflymsc;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
+import com.iflytek.cloud.InitListener;
 import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechRecognizer;
 import com.iflytek.cloud.SpeechUtility;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 import dongzhong.utilforiflymsc.configs.Configs;
 import dongzhong.utilforiflymsc.exceptions.InitException;
@@ -15,12 +21,39 @@ import dongzhong.utilforiflymsc.exceptions.InitException;
 public class IflyManager {
     private static IflyManager instance;
 
+    private Context context;
+    private SpeechRecognizer speechRecognizer;
+
     private IflyManager(Context context) throws InitException {
         String appid = Configs.getAppId();
         if (appid == null) {
             throw new InitException("appid is null");
         }
+        this.context = context;
         SpeechUtility.createUtility(context, SpeechConstant.APPID +"=" + appid + "," + SpeechConstant.ENGINE_MODE + "=" + SpeechConstant.MODE_MSC);
+        speechRecognizer = SpeechRecognizer.createRecognizer(context, new InitListener() {
+            @Override
+            public void onInit(int i) {
+
+            }
+        });
+        initSpeechRecognizer();
+    }
+
+    private void initSpeechRecognizer() {
+        if (speechRecognizer != null) {
+            // 清空参数
+            speechRecognizer.setParameter(SpeechConstant.PARAMS, null);
+
+            speechRecognizer.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
+            speechRecognizer.setParameter(SpeechConstant.VAD_ENABLE, "1");
+            speechRecognizer.setParameter(SpeechConstant.VAD_BOS, "8000");
+            speechRecognizer.setParameter(SpeechConstant.VAD_EOS, "500");
+            speechRecognizer.setParameter(SpeechConstant.ASR_PTT, "0");
+            speechRecognizer.setParameter(SpeechConstant.RESULT_TYPE, "json");
+            speechRecognizer.setParameter(SpeechConstant.MIXED_THRESHOLD, "60");
+            speechRecognizer.setParameter(SpeechConstant.NET_TIMEOUT, "2000");
+        }
     }
 
     public static IflyManager getInstance(Context context) throws InitException {
@@ -32,5 +65,14 @@ public class IflyManager {
             }
         }
         return instance;
+    }
+
+    public void startRecognize(@NonNull String fileName) {
+        speechRecognizer.setParameter(SpeechConstant.AUDIO_SOURCE, "-2");
+        speechRecognizer.setParameter(SpeechConstant.ASR_SOURCE_PATH, fileName);
+        File file = new File(fileName);
+        if (!file.exists()) {
+            return;
+        }
     }
 }
