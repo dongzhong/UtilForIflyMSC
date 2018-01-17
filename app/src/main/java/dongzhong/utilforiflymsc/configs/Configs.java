957111069f7184ee563543b3a4e930cc92ecd250
董重 <zhong.dong@flyingwings.cn>
2018-01-17 16:13:49 +0800

    从文件中读取appid（文件位置/mnt/sdcard/ifly/appid.txt）

---
+++ app/src/main/java/dongzhong/utilforiflymsc/configs/Configs.java
@@ -0,0 +1,59 @@
+package dongzhong.utilforiflymsc.configs;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by dongzhong on 2018/1/17.
+ */
+
+public class Configs {
+    private final static String SdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private final static String filePath = "ifly";
+    private final static String fileName = "appid.txt";
+
+    private static File fileDir;
+    private static File file;
+    private static FileInputStream fileInputStream;
+    static {
+        fileDir = new File(SdPath, filePath);
+        if (!fileDir.exists()) {
+            fileDir.mkdir();
+        }
+        file = new File(fileDir, fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            }
+            catch (Exception e) {
+
+            }
+        }
+        try {
+            fileInputStream = new FileInputStream(file);
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    public static String getAppId() {
+        if (fileInputStream == null) {
+            return null;
+        }
+        try {
+            int length = fileInputStream.available();
+            byte[] buffer = new byte[length];
+            fileInputStream.read(buffer);
+            String appid = new String(buffer);
+            return appid;
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+}
